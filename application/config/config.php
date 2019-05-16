dd76e6995e285bd416de210aef48c3651340535e
arnaudmaesboyer <43748615+arnaudmaesboyer@users.noreply.github.com>
2019-05-17 00:51:59 +0200

    edit encryption

--- application/config/config.php
+++ application/config/config.php
@@ -324,7 +324,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '2D9TFIO4DSC8DS4CSD875TF9FVESH';
+$config['encryption_key'] = '2D9TFIO4DSC8DS4CSD875TF9FVES';
 
 /*
 |--------------------------------------------------------------------------
