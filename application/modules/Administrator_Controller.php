74c2c9492e09d0e568c9e88fa91aa4ace7b21d7d
arnaudmaesboyer <43748615+arnaudmaesboyer@users.noreply.github.com>
2019-05-17 00:04:06 +0200

    edit encryption

--- application/modules/Administrator_Controller.php
+++ application/modules/Administrator_Controller.php
@@ -20,7 +20,7 @@ class ADMINISTRATOR_Controller extends CI_Controller {
     {
         parent::__construct();
         $this->load->helper('cookie');
-        $this->load->library('encrypt');
+        $this->load->library('encryption');
         $this->load->model('administrator_model');
         
         // if route fetch tu veux aller 
@@ -30,14 +30,14 @@ class ADMINISTRATOR_Controller extends CI_Controller {
             {
                 $cookies_identifiant = $this->_cookie;
                 $cookies_identifiant['name'] = $this->_cookie_id_name;
-                $cookies_identifiant['value'] = $this->encrypt->encode($this->input->post('identifiant'));
+                $cookies_identifiant['value'] = $this->encryption->encode($this->input->post('identifiant'));
                 // $cookies_identifiant['domain'] = "";
                 $cookies_identifiant['prefix'] = $this->config->item('cookie_prefix');
                 set_cookie($cookies_identifiant);
 
                 $cookies_password = $this->_cookie;
                 $cookies_password['name'] = $this->_cookie_id_password;
-                $cookies_password['value'] = $this->encrypt->encode($this->input->post('password'));
+                $cookies_password['value'] = $this->encryption->encode($this->input->post('password'));
                 // $cookies_identifiant['domain'] = "";
                 $cookies_password['prefix'] = $this->config->item('cookie_prefix');
                 set_cookie($cookies_password);
@@ -49,14 +49,14 @@ class ADMINISTRATOR_Controller extends CI_Controller {
             elseif($this->administrator_model->validateClient($this->input->post('identifiant'), $this->input->post('password'))){
                 $cookies_identifiant = $this->_cookie;
                 $cookies_identifiant['name'] = $this->_cookie_id_nameClient;
-                $cookies_identifiant['value'] = $this->encrypt->encode($this->input->post('identifiant'));
+                $cookies_identifiant['value'] = $this->encryption->encode($this->input->post('identifiant'));
                 // $cookies_identifiant['domain'] = "";
                 $cookies_identifiant['prefix'] = $this->config->item('cookie_prefix');
                 set_cookie($cookies_identifiant);
 
                 $cookies_password = $this->_cookie;
                 $cookies_password['name'] = $this->_cookie_id_passwordClient;
-                $cookies_password['value'] = $this->encrypt->encode($this->input->post('password'));
+                $cookies_password['value'] = $this->encryption->encode($this->input->post('password'));
                 // $cookies_identifiant['domain'] = "";
                 $cookies_password['prefix'] = $this->config->item('cookie_prefix');
                 set_cookie($cookies_password);
@@ -73,8 +73,8 @@ class ADMINISTRATOR_Controller extends CI_Controller {
         elseif (get_cookie($this->config->item('cookie_prefix').$this->_cookie_id_name, TRUE) &&
                 get_cookie($this->config->item('cookie_prefix').$this->_cookie_id_password, TRUE))
         {
-            $mail = $this->encrypt->decode(get_cookie($this->config->item('cookie_prefix').$this->_cookie_id_name));
-            $password = $this->encrypt->decode(get_cookie($this->config->item('cookie_prefix').$this->_cookie_id_password));
+            $mail = $this->encryption->decode(get_cookie($this->config->item('cookie_prefix').$this->_cookie_id_name));
+            $password = $this->encryption->decode(get_cookie($this->config->item('cookie_prefix').$this->_cookie_id_password));
             if ($this->administrator_model->validate($mail, $password) == FALSE){
                 //redirect(site_url("connexion")); 
             }
@@ -84,8 +84,8 @@ class ADMINISTRATOR_Controller extends CI_Controller {
         elseif (get_cookie($this->config->item('cookie_prefix').$this->_cookie_id_nameClient, TRUE) &&
                 get_cookie($this->config->item('cookie_prefix').$this->_cookie_id_passwordClient, TRUE))
         {
-            $mailClient = $this->encrypt->decode(get_cookie($this->config->item('cookie_prefix').$this->_cookie_id_nameClient));
-            $passwordClient = $this->encrypt->decode(get_cookie($this->config->item('cookie_prefix').$this->_cookie_id_passwordClient));
+            $mailClient = $this->encryption->decode(get_cookie($this->config->item('cookie_prefix').$this->_cookie_id_nameClient));
+            $passwordClient = $this->encryption->decode(get_cookie($this->config->item('cookie_prefix').$this->_cookie_id_passwordClient));
             if ($this->administrator_model->validateClient($mailClient, $passwordClient) == FALSE){
                 //redirect(site_url("connexion")); 
             }     // Mauvais identifiant, ont redirige vers la page de connexion
@@ -114,7 +114,7 @@ class ADMINISTRATOR_Controller extends CI_Controller {
 	public function isAdmin(){
         $test= $this->input->cookie("189CDS8CSDC98JCPDSCDSCDSCDSD8C9SD");
         if(isset($test)){
-        $mail = $this->encrypt->decode($this->input->cookie("189CDS8CSDC98JCPDSCDSCDSCDSD8C9SD"));
+        $mail = $this->encryption->decode($this->input->cookie("189CDS8CSDC98JCPDSCDSCDSCDSD8C9SD"));
         $this->load->model("Administrator_model");
         return $this->Administrator_model->isAdmin($mail);
         }
@@ -123,7 +123,7 @@ class ADMINISTRATOR_Controller extends CI_Controller {
     public function isClient(){
         $test= $this->input->cookie("289CDS8CSDC98JCPDSCDSCDSCDSD8C9SD");
         if(isset($test)){
-        $mailClient = $this->encrypt->decode($this->input->cookie("289CDS8CSDC98JCPDSCDSCDSCDSD8C9SD"));
+        $mailClient = $this->encryption->decode($this->input->cookie("289CDS8CSDC98JCPDSCDSCDSCDSD8C9SD"));
         $this->load->model("Administrator_model");
         return $this->Administrator_model->isClient($mailClient);
         }
