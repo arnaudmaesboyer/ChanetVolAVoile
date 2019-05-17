bad14220766bb80499001f106fc198d4581b4815
arnaudmaesboyer <43748615+arnaudmaesboyer@users.noreply.github.com>
2019-05-17 20:35:56 +0200

    bug

--- application/controllers/GestionPlaneurs.php
+++ application/controllers/GestionPlaneurs.php
@@ -65,7 +65,7 @@ class GestionPlaneurs extends Administrator_Controller {
 
                 $this->upload->do_upload('Image');
         $this->GestionPlaneurs_model->AjoutPlaneur();
-        redirect(site_url("GestionPlaneur"));
+        redirect(site_url("GestionPlaneurs"));
 
         
     }
