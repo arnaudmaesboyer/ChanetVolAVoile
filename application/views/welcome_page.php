a3f61472bfb481db3e96a914a82e274b5bd22645
arnaudmaesboyer <43748615+arnaudmaesboyer@users.noreply.github.com>
2019-05-19 12:50:34 +0200

    rajout cate

--- application/views/welcome_page.php
+++ application/views/welcome_page.php
@@ -48,8 +48,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           </a>
       </div>
   </div>
-  <div class="row justify-content-md-center">
-  <h1 class="mt-3 mb-2 col-8"> Ou nous trouver ?</h1>
+  <div class="justify-content-md-center">
+  <h1 class=" mt-3 mb-2 col-8"> Ou nous trouver ?</h1>
   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d217460.82534294!2d3.333018710121552!3d44.2643074850193!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b39f2227bdfdcf%3A0x3df331e001653660!2sChanet+Vol+%C3%A0+voile!5e0!3m2!1sfr!2sfr!4v1558261843656!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
 </div>
